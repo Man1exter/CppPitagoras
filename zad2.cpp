4c1c00ace8d4ba5d677bdc8b616877fc012ef793
Mariusz Perzynski <m.perzyk@interia.pl>
2020-11-04 10:14:58 +0100

    done 2 exercises to exam for study

--- zad2.cpp
+++ zad2.cpp
@@ -6,8 +6,28 @@ int main(){
 
 float a,b,c;
 
+cout<<"Podaj wartości i zobacz największą wartość z podanych"<< endl;
 
+cout<<"Podaj wartość a: "<<endl;
+cin >> a;
 
+cout<<"Podaj wartość b: "<<endl;
+cin >> b;
+
+cout<<"Podaj wartość c: "<<endl;
+cin >> c;
+
+if ((a > b) && (a > c)) {
+
+cout<< "największa wartość to a: "<< a <<endl;
+
+} if ((b > a) && (b > c)) {
+
+cout<< "największa wartość to b: "<< b <<endl;
+
+} else {
+cout<< "największa wartość to c: "<< c <<endl;
+}
 
 
 
