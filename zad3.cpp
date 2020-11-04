48f6138dade0f85ef3ad773ae7fada37e498bd37
Mariusz Perzynski <m.perzyk@interia.pl>
2020-11-04 14:06:05 +0100

    complete all exercises tasks

--- zad3.cpp
+++ zad3.cpp
@@ -6,9 +6,9 @@ using namespace std;
 int main()
 {
 
-int a,b,c;
-int delta;
-int x1,x2;
+int a,b,c,delta,x1,x2,elp;
+
+
 
    cout<<"podaj wartość a:"<< endl;
    cin>>a;
@@ -22,17 +22,20 @@ delta = b*b-4*a*c;
 
 if (delta < 0){
     cout<<"Liczba pierwiastków równa się   0"<<endl;
+    elp = 0;
 }
 if (delta == 0){
     cout<<"Liczba pierwiastków równa się   1"<<endl;
+    elp = 1;
 }
 if (delta > 0){
     cout<<"Liczba pierwiastków równa się   2"<<endl;
+    elp = 2;
     // 2 10 8 a b c
 }
 
 
-switch (delta)
+switch (elp)
 {
     case 0:
    cout << "brak pierwiastkow rzeczywistych." << endl;
@@ -40,15 +43,15 @@ break;
 
     case 1:
    x1 = -b/(2*a);
-   cout<<"x1 równa się"<<x1<<endl;
+   cout<<"x1 równa się"<< x1 <<endl;
 break;
 
     case 2:
    x1 = (-b-sqrt(delta))/(2*a); 
    x2 = (-b+sqrt(delta))/(2*a);
    
-   cout<<"x1 równa się"<<x1<<endl;
-   cout<<"x2 równa się"<<x2<<endl;
+   cout<<"x1 równa się"<< x1 <<endl;
+   cout<<"x2 równa się"<< x2 <<endl;
 break;
 
 }
